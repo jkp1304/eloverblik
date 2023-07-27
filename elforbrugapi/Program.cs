3bfa279ec3053d99d61705ed94ee9f365323fcd6
Jesper <1078031@ucn.dk>
2023-07-27 10:16:47 +0200

    oprydning og udkommentering af kode.

--- elforbrugapi/Program.cs
+++ elforbrugapi/Program.cs
@@ -3,8 +3,6 @@ using System;
 using System.Net.Http;
 using System.Net.NetworkInformation;
 using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
 using MySqlConnector;
 using System.Security.Cryptography;
 using Microsoft.VisualBasic;
